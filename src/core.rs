ababdcf352e8fe705d0c779e3b95ac7dbd9f1b0b
Emma <felix.hppt@gmail.com>
2025-07-21 01:25:13 +0200

    Define cpu Core, Bus and decode opcodes

---
+++ src/core.rs
@@ -0,0 +1,133 @@
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct Core {
+    pub a: u16,
+    pub d: u16,
+    pub dbr: u8,
+    pub e: bool,
+    pub p: P,
+    pub pbr: u8,
+    pub pc: u16,
+    pub s: u16,
+    pub x: u16,
+    pub y: u16,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct P(u8);
+impl P {
+    pub fn c(self) -> bool {
+        self.0 & Self::C != 0
+    }
+    pub fn z(self) -> bool {
+        self.0 & Self::Z != 0
+    }
+    pub fn i(self) -> bool {
+        self.0 & Self::I != 0
+    }
+    pub fn d(self) -> bool {
+        self.0 & Self::D != 0
+    }
+    pub fn x(self) -> bool {
+        self.0 & Self::X != 0
+    }
+    pub fn m(self) -> bool {
+        self.0 & Self::M != 0
+    }
+    pub fn v(self) -> bool {
+        self.0 & Self::V != 0
+    }
+    pub fn n(self) -> bool {
+        self.0 & Self::N != 0
+    }
+
+    pub fn set_c(&mut self, to: bool) {
+        self.0 &= !Self::C;
+        if to {
+            self.0 |= Self::C
+        }
+    }
+    pub fn set_z(&mut self, to: bool) {
+        self.0 &= !Self::Z;
+        if to {
+            self.0 |= Self::Z
+        }
+    }
+    pub fn set_i(&mut self, to: bool) {
+        self.0 &= !Self::I;
+        if to {
+            self.0 |= Self::I
+        }
+    }
+    pub fn set_d(&mut self, to: bool) {
+        self.0 &= !Self::D;
+        if to {
+            self.0 |= Self::D
+        }
+    }
+    pub fn set_x(&mut self, to: bool) {
+        self.0 &= !Self::X;
+        if to {
+            self.0 |= Self::X
+        }
+    }
+    pub fn set_m(&mut self, to: bool) {
+        self.0 &= !Self::M;
+        if to {
+            self.0 |= Self::M
+        }
+    }
+    pub fn set_v(&mut self, to: bool) {
+        self.0 &= !Self::V;
+        if to {
+            self.0 |= Self::V
+        }
+    }
+    pub fn set_n(&mut self, to: bool) {
+        self.0 &= !Self::N;
+        if to {
+            self.0 |= Self::N
+        }
+    }
+
+    pub fn with_c(mut self, to: bool) -> Self {
+        self.set_c(to);
+        self
+    }
+    pub fn with_z(mut self, to: bool) -> Self {
+        self.set_z(to);
+        self
+    }
+    pub fn with_i(mut self, to: bool) -> Self {
+        self.set_i(to);
+        self
+    }
+    pub fn with_d(mut self, to: bool) -> Self {
+        self.set_d(to);
+        self
+    }
+    pub fn with_x(mut self, to: bool) -> Self {
+        self.set_x(to);
+        self
+    }
+    pub fn with_m(mut self, to: bool) -> Self {
+        self.set_m(to);
+        self
+    }
+    pub fn with_v(mut self, to: bool) -> Self {
+        self.set_v(to);
+        self
+    }
+    pub fn with_n(mut self, to: bool) -> Self {
+        self.set_n(to);
+        self
+    }
+
+    const C: u8 = 1;
+    const Z: u8 = 2;
+    const I: u8 = 4;
+    const D: u8 = 8;
+    const X: u8 = 16;
+    const M: u8 = 32;
+    const V: u8 = 64;
+    const N: u8 = 128;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,89 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use crate::core::Core;
+
+pub mod core;
+pub mod instruction;
+
+pub struct Bus {
+    pub addr: u16,
+    pub bank: u8,
+    pub data: u8,
+    flags: u8,
+}
+impl Bus {
+    pub fn irq(self) -> bool {
+        self.flags & Self::IRQ != 0
+    }
+    pub fn nmi(self) -> bool {
+        self.flags & Self::NMI != 0
+    }
+    pub fn rdy(self) -> bool {
+        self.flags & Self::RDY != 0
+    }
+    pub fn res(self) -> bool {
+        self.flags & Self::RES != 0
+    }
+    pub fn rw(self) -> bool {
+        self.flags & Self::RW != 0
+    }
+    pub fn vda(self) -> bool {
+        self.flags & Self::VDA != 0
+    }
+    pub fn vpa(self) -> bool {
+        self.flags & Self::VPA != 0
+    }
+
+    pub fn set_irq(&mut self, to: bool) {
+        self.flags &= !Self::IRQ;
+        if to {
+            self.flags |= Self::IRQ;
+        }
+    }
+    pub fn set_nmi(&mut self, to: bool) {
+        self.flags &= !Self::NMI;
+        if to {
+            self.flags |= Self::NMI;
+        }
+    }
+    pub fn set_rdy(&mut self, to: bool) {
+        self.flags &= !Self::RDY;
+        if to {
+            self.flags |= Self::RDY;
+        }
+    }
+    pub fn set_res(&mut self, to: bool) {
+        self.flags &= !Self::RES;
+        if to {
+            self.flags |= Self::RES;
+        }
+    }
+    pub fn set_rw(&mut self, to: bool) {
+        self.flags &= !Self::RW;
+        if to {
+            self.flags |= Self::RW;
+        }
+    }
+    pub fn set_vda(&mut self, to: bool) {
+        self.flags &= !Self::VDA;
+        if to {
+            self.flags |= Self::VDA;
+        }
+    }
+    pub fn set_vpa(&mut self, to: bool) {
+        self.flags &= !Self::VPA;
+        if to {
+            self.flags |= Self::VPA;
+        }
+    }
+
+    const IRQ: u8 = 1;
+    const NMI: u8 = 2;
+    const RDY: u8 = 4;
+    const RES: u8 = 8;
+    const RW: u8 = 16;
+    const VDA: u8 = 32;
+    const VPA: u8 = 64;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub struct Cpu65816 {
+    core: Core,
 }
